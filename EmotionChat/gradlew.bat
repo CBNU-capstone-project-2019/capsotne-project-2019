7468cd080746a182c477ce7cb015c7442a5bf2ba
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2019-11-21 16:24:32 +0900

    android

---
+++ EmotionChat/gradlew.bat
@@ -0,0 +1,38 @@
+<application>
+  <component name="RecentProjectsManager">
+    <option name="recentPaths">
+      <list>
+        <option value="$USER_HOME$/AndroidStudioProjects/APIExam" />
+        <option value="$USER_HOME$/Desktop/2018_2/모프/기말/MyApplication2/MyApplication2" />
+        <option value="$USER_HOME$/AndroidStudioProjects/myAPIexam" />
+        <option value="$USER_HOME$/Desktop/2018_2/모프/MyApplication (2)/MyApplication/MyApplication" />
+        <option value="$USER_HOME$/AndroidStudioProjects/MyApplication4" />
+        <option value="$USER_HOME$/Desktop/2018_2/모프/MyApplication (2)/MyApplication" />
+        <option value="$USER_HOME$/AndroidStudioProjects/test1" />
+        <option value="$USER_HOME$/MyApplication" />
+        <option value="D:/2016037033조다운/MyApplication/MyApplication" />
+        <option value="D:/2016037033조다운/MyApplication" />
+        <option value="D:/2016037033조다운" />
+        <option value="$USER_HOME$/AndroidStudioProjects/mysearch" />
+        <option value="$USER_HOME$/AndroidStudioProjects/listviewsearch" />
+        <option value="$USER_HOME$/AndroidStudioProjects/Myapplication5" />
+        <option value="$USER_HOME$/AndroidStudioProjects/android181017" />
+        <option value="$USER_HOME$/AndroidStudioProjects/android181010" />
+        <option value="$USER_HOME$/AndroidStudioProjects" />
+        <option value="$USER_HOME$/AndroidStudioProjects/new" />
+        <option value="$USER_HOME$/AndroidStudioProjects/MyApplication2" />
+        <option value="$USER_HOME$/AndroidStudioProjects/MyApplication3" />
+        <option value="$USER_HOME$/AndroidStudioProjects/android181017_2" />
+        <option value="$USER_HOME$/Desktop/CustomListview/MyApplication2" />
+        <option value="$USER_HOME$/Desktop/CustomListview/MyApplication3" />
+        <option value="$USER_HOME$/Desktop/CustomListview/MyApplication" />
+        <option value="$USER_HOME$/Desktop/CustomListview" />
+        <option value="$USER_HOME$/AndroidStudioProjects/MyApplication" />
+      </list>
+    </option>
+    <option name="openPaths">
+      <list>
+        <option value="$USER_HOME$/AndroidStudioProjects/APIExam" />
+      </list>
+    </option>
+    <option name="lastPath" value="$USER_HOME$/AndroidStudioProjects/A
